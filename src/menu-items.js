884c198a7e181dff7a463ee45de939e8bc55619d
adifdwimaulana <adif.maulana@thebonza.com>
2021-09-15 03:12:59 +0700

    fix: update routing based on role

--- src/menu-items.js
+++ src/menu-items.js
@@ -1,58 +1,103 @@
-export default {
-    items: [
-        {
-            id: 'navigation',
-            title: 'Navigation',
-            type: 'group',
-            icon: 'icon-navigation',
-            children: [
-                {
-                    id: 'dashboard',
-                    title: 'Dashboard',
-                    type: 'item',
-                    url: '/dashboard',
-                    icon: 'feather icon-home',
-                },
-                {
-                    id: 'management',
-                    title: 'Manajemen',
-                    type: 'collapse',
-                    icon: 'feather icon-file-text',
-                    children: [
-                        {
-                            id: 'patient',
-                            title: 'Anggota',
-                            type: 'item',
-                            url: '/management/patient'
-                        },
-                        {
-                            id: 'medicine',
-                            title: 'Obat',
-                            type: 'item',
-                            url: '/management/medicine'
-                        },
-                        {
-                            id: 'request',
-                            title: 'Pemeriksaan',
-                            type: 'item',
-                            url: '/management/request'
-                        },
-                        {
-                            id: 'reqmed',
-                            title: 'Permintaan Obat',
-                            type: 'item',
-                            url: '/management/reqmed'
-                        }
-                    ]
-                },
-                {
-                    id: 'logout',
-                    title: 'Keluar',
-                    type: 'item',
-                    icon: 'feather icon-log-out',
-                    url: '/logout'
-                }
-            ]
-        }
-    ]
+import cookie from 'react-cookies'
+
+let route;
+
+if(cookie.load('roleId') < 3){
+    route = {
+        items: [
+            {
+                id: 'navigation',
+                title: 'Navigation',
+                type: 'group',
+                icon: 'icon-navigation',
+                children: [
+                    {
+                        id: 'dashboard',
+                        title: 'Dashboard',
+                        type: 'item',
+                        url: '/dashboard',
+                        icon: 'feather icon-home',
+                    },
+                    {
+                        id: 'management',
+                        title: 'Manajemen',
+                        type: 'collapse',
+                        icon: 'feather icon-file-text',
+                        children: [
+                            {
+                                id: 'patient',
+                                title: 'Anggota',
+                                type: 'item',
+                                url: '/management/patient'
+                            },
+                            {
+                                id: 'request',
+                                title: 'Pemeriksaan',
+                                type: 'item',
+                                url: '/management/request'
+                            },
+                        ]
+                    },
+                    {
+                        id: 'logout',
+                        title: 'Keluar',
+                        type: 'item',
+                        icon: 'feather icon-log-out',
+                        url: '/logout'
+                    }
+                ]
+            }
+        ]
+    }
+} else {
+    route = {
+        items: [
+            {
+                id: 'navigation',
+                title: 'Navigation',
+                type: 'group',
+                icon: 'icon-navigation',
+                children: [
+                    {
+                        id: 'dashboard',
+                        title: 'Dashboard',
+                        type: 'item',
+                        url: '/dashboard',
+                        icon: 'feather icon-home',
+                    },
+                    {
+                        id: 'management',
+                        title: 'Manajemen',
+                        type: 'collapse',
+                        icon: 'feather icon-file-text',
+                        children: [
+                            {
+                                id: 'medicine',
+                                title: 'Obat',
+                                type: 'item',
+                                url: '/management/medicine'
+                            },
+                            {
+                                id: 'reqmed',
+                                title: 'Permintaan Obat',
+                                type: 'item',
+                                url: '/management/reqmed'
+                            }
+                        ]
+                    },
+                    {
+                        id: 'logout',
+                        title: 'Keluar',
+                        type: 'item',
+                        icon: 'feather icon-log-out',
+                        url: '/logout'
+                    }
+                ]
+            }
+        ]
+    }
 }
+
+
+
+export default route
--- src/redux/actions/users/auth.js
+++ src/redux/actions/users/auth.js
@@ -139,44 +139,9 @@ export function fetchLogout(){
             }
         })
         .catch(function(error){
-            if (error.response) {
-                if(error.response.status === 401) {
-                    dispatch({
-                        type: FAILED_LOGOUT
-                    })
-                    return toast.error(error.response.data.message);
-                } else if (error.response.status === 403) {
-                    dispatch({
-                        type: FAILED_LOGOUT
-                    })
-                    return toast.error(error.response.data.message);
-                } else if (error.response.status === 400) {
-                    dispatch({
-                        type: FAILED_LOGOUT
-                    })
-                    return toast.error(error.response.data.message);
-                } else if (error.response.status === 404 || error.response.status === 500) {
-                    dispatch({
-                        type: FAILED_LOGOUT
-                    })
-                    return toast.error("Server cannot be contacted! Please ask your system administrator!");
-                } else {
-                    dispatch({
-                        type: FAILED_LOGOUT
-                    })
-                    return toast.error('Something went wrong... Please try again later...')
-                }
-            } else if (error.request) {
-                dispatch({
-                    type: FAILED_LOGOUT
-                })
-                return toast.error('Request have no response! Please check on your internet connection and refresh this page.')
-            } else {
-                dispatch({
-                    type: FAILED_LOGOUT
-                })
-                return toast.error('Something went wrong... Please try again later...')
-            }
+            dispatch({
+                type: FAILED_LOGOUT
+            })
         })
     }
 }
